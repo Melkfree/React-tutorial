0826e1229a9b3a5955dabb6c70f4d1fec0be3466
Melkfree <yevhen.ihnatiuk@gmail.com>
2021-10-23 13:45:25 +0300

    two-way binding input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(5);
+  const [value, setValue] = useState('Some text');
+  
+  // function inputChange(){
+  //   setValue(value = event.target.value);
+  // }
+
+  function increment(){
+    setCount(count + 1);
+    console.log(count);
+  }
+
+  function decrement(){
+    setCount(count - 1);
+    console.log(count);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Faster
-        </a>
-      </header>
+      <h1>{count}</h1>
+
+      <h1>{value}</h1>
+      <input type="text" value={value} onChange={(event)=>{setValue(event.target.value)}}/>
+      <button onClick={increment} onChange={increment}>Increment</button>
+      <button onClick={decrement} onChange={decrement}>Decrement</button>
     </div>
   );
 }
