47ff529ab7bf802fabfff69089bd8519c99b6dd1
Melkfree <yevhen.ihnatiuk@gmail.com>
2021-10-24 10:49:18 +0300

    Counter component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
 import React, { useState } from "react";
+import Counter from "./Components/Counter";
 
 function App() {
 
-  const [count, setCount] = useState(5);
   const [value, setValue] = useState('Some text');
   
-  // function inputChange(){
-  //   setValue(value = event.target.value);
-  // }
-
-  function increment(){
-    setCount(count + 1);
-    console.log(count);
-  }
-
-  function decrement(){
-    setCount(count - 1);
-    console.log(count);
-  }
-
   return (
     <div className="App">
-      <h1>{count}</h1>
-
-      <h1>{value}</h1>
-      <input type="text" value={value} onChange={(event)=>{setValue(event.target.value)}}/>
-      <button onClick={increment} onChange={increment}>Increment</button>
-      <button onClick={decrement} onChange={decrement}>Decrement</button>
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
   );
 }
